1d57cfafa57684e13ee0d878c22cbc324496df74
Nia K <nsjkappen@gmail.com>
2021-08-05 07:28:49 -0700

    updated

--- index.js
+++ index.js
@@ -1,67 +1,57 @@
 // variables and constants
 
-const cartContainer = document.querySelector('.cart-container');
-const productList = document.querySelector('.product-list');
+
+const productContainer = document.getElementById("product-container")
+//const productList= document.querySelector(".product -list")
+//console.log(productContainer)
+
+//load books content from JSON file 
 
 
-
-eventListeners();
-
-function eventListeners(){
-    window.addEventListener('DOMContentLoaded', () =>{ //The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
-        loadJSON()
-    });
-
-
-document.querySelector('.navbar-toggler').addEventListener('click',() =>{
-    document.querySelector('.navbar-collapse').classList.toggle('show-navbar');
-
-});
-
-//show/hide cart container
-
-document.getElementById('cart-btn').addEventListener('click',() => {
-    cartContainer.classList.toggle('show-cart-container');
-});
-
-}
-
-// var myInit = {method: 'GET',
-// headers: {
-//     'Content-Type': 'application/json'
-// },
-// mode: 'cors',
-// cache: 'default'};
-
-// let myRequest = new Request("./data_class.json", myInit);
-
-
-
-function loadJSON(){
-    fetch('http://localhost:5500/books.json')
+   
+fetch('http://localhost:5500/books.json')
     .then(response => response.json())
     .then(data => {
-        let html = ' ';
-        data.forEach(product => {
+        let html = '';
+        data.forEach(books => {
             html += `
+
             <div class = "product-item">
-                <div class = "product-img">
-                    <img src = "${product.image}" alt = "product image">
-                    <button type = "button" class = "add-to-cart-btn">
-                        <i class = "fas fa-shopping-cart"></i>Add To Cart
-                    </button>
-                </div>
-                <div class = "product-content">
-                    <h3 class = "product-name">${product.title}</h3>
-                    <span class = "product-category">${product.genre}</span>
-                    <p class = "product-price">$${product.price}</p>
-                </div>
+            <div class = "product-img">
+            <img src = "${books.image}" alt = "books image">
+            <button type = "button" class = "add-to-cart-btn">
+                <i class = "fas fa-shopping-cart"></i>Add To Cart
+                </button>
             </div>
-        `;
-    });
-    productList.innerHTML = html;
-})
-    
-}
+
+            <div class = "product-content">
+            <h3 class = "product-name">${books.title}</h3>
+            <h3 class = "product-author">${books.author}</h3>
+            <span class = "product-category">${books.genre}</span>
+            <p class = "product-price">$${books.price}</p>
+
+            </div>
+            </div>
+
+
+        
+         `
+        })
+
+        productContainer.innerHTML = html
+    })
+    .catch(error => {
+        alert(`User live server or local server`);
+        //URL scheme must be "http" or "https" for CORS request. You need to be serving your index.html locally or have your site hosted on a live server somewhere for the Fetch API to work properly.
+    })
+
+    function purchaseProduct(e){
+        if(e.target.classList.contains('add-to-cart-btn')){
+            let product = e.target.parentElement.parentElement;
+            getProductInfo(product);
+        }
+    }
+
+
 
 
